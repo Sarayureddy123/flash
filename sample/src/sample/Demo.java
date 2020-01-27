59d019a0afa8f1a62fe1fdc1ec1b668363e53390
Sarayureddy123 <60181904+Sarayureddy123@users.noreply.github.com>
2020-01-27 19:43:09 +0530

    first commit uploading all files

---
+++ sample/src/sample/Demo.java
@@ -0,0 +1,17 @@
+package sample;
+
+public class Demo {
+	public static void main(String[] args) {
+		int input=10;
+		int finalresult=summation(input);
+		System.out.print(finalresult);
+		
+	}
+	public static int summation(int n)
+	{
+		int result=(n*(n+1))/2;
+		return result;
+	
+	}
+
+}
